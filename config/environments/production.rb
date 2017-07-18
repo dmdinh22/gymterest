e417784dc5ffe2d953a2b17a166e84f6e3cf95af
David Dinh <dmdinh22@gmail.com>
2017-07-18 15:29:11 -0700

    aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,9 +98,8 @@ Rails.application.configure do
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  },
-  :s3_host_name => "s3-us-west-1.amazonaws.com", 
-  :url => ":s3_host_name" 
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_region: ENV.fetch('AWS_REGION'),
+  }
 }
 end
