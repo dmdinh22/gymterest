cddfc746a13a26ac1bc7d9e6e5b135a1e855a4f4
David Dinh <dmdinh22@gmail.com>
2017-07-18 17:11:36 -0700

    old ROR proj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery_ujs
+//= require bootstrap
 //= require jquery
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
